04204a4513982a615035613fe367b57a9283f3f1
Teemu Hirsikangas <teemu.hirsikangas@hubchat.com>
2017-04-06 15:26:55 +0300

    add missing value rounding ton technical room temp value

--- scripts/send_technicalroom_temp.py
+++ scripts/send_technicalroom_temp.py
@@ -11,6 +11,7 @@ import Adafruit_DHT
 sensor = Adafruit_DHT.AM2302
 pin = 4
 technical_humid, technical_room = Adafruit_DHT.read_retry(sensor,pin)
+technical_room = round(float(technical_room),1)
 technical_humid = round(float(technical_humid),1)
 
 url = 'http://numberpi.local:3333/homeautomation/technicalroom'
