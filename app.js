8596b1f7ebeed8f6f48932b6f2162f08f2cf1170
Teemu Hirsikangas <thirsikangas@opentext.com>
2024-07-05 14:40:57 +0300

    update deps, remove unused ones

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 'use strict';
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sqlite3 = require('sqlite3').verbose();
@@ -38,9 +36,6 @@ app.set('view engine', 'pug');
 // might be security problems if mirror is in public internet, as it exposes all
 // -> expose only modules needed
 app.use(express.static(__dirname + '/node_modules'));  
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
